30ce9b2652607799fe6040a8d049bcfc6ad558cc
rsleiva <rsleiva@edenor.com>
2024-08-13 14:10:41 -0300

    frontend_1.0.4
    Se agrega la vista Manager donde se gestionaran los cortes y edp relacionados

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './Login';  // Asegúrate de que Login.js existe en la misma carpeta
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './login/Login';
+import Manager from './afectaciones/Manager';  // Importa el componente Manager
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Redirigir desde la ruta raíz a /SGE/Login */}
+        <Route path="/" element={<Navigate to="/SGE/Login" />} />
         <Route path="/SGE/Login" element={<Login />} />
+        <Route path="/SGE/Manager" element={<Manager />} />  {/* Ruta para Manager */}
       </Routes>
     </Router>
   );
---
+++ frontend/src/afectaciones/Manager.js
@@ -0,0 +1,13 @@
+// src/afectaciones/Manager.js
+import React from 'react';
+
+function Manager() {
+  return (
+    <div>
+      <h1>Manager Page</h1>
+      {/* Otros contenidos de la página */}
+    </div>
+  );
+}
+
+export default Manager;  // Exportación por defecto
