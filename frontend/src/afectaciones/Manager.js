9ecf61e98011f753280a6784c3823dfc03df8709
robertosl77 <robertosl77@gmail.com>
2024-08-22 01:09:55 -0300

    frontend_1.0.6
    se intento poner un dashboard

--- frontend/src/afectaciones/Manager.js
+++ frontend/src/afectaciones/Manager.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import Navbar from '../navegacion/Navbar';  // Ajusta la ruta según la estructura de tu proyecto
+import Navbar from '../navegacion/Navbar';
+import './css/Dashboard.css';
 
 function Manager() {
   return (
     <div>
       <Navbar />
-      <div className="content" style={{ marginTop: '60px' }}>
-        <h1>Manager Page</h1>
-        {/* Contenido de la página */}
-      </div>
+        <div className="dashboard">
+          <div className="dashboard-card">EDP Afectados: 15</div>
+          <div className="dashboard-card">Reclamos: 5</div>
+          <div className="dashboard-card">Sin Llamar: 10</div>
+          <div className="dashboard-card">Sin Autonomía: 2</div>
+          <div className="dashboard-card">Sin FAE: 12</div>
+          <div className="dashboard-card">Sin AMI: 8</div>
+          <div className="dashboard-card">Seguimiento: 2</div>
+        </div>
+        <div className="content">
+          <h1>Manager Page</h1>
+          <p>Acá está reservado para colocar una tabla, pero a futuro</p>
+        </div>
     </div>
   );
 }
