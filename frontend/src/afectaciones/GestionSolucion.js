bb663f4352437bd8d98a5b6a848eaf11563433d4
robertosl77 <robertosl77@gmail.com>
2024-09-04 20:58:25 -0300

    frontend_1.4.3
    Se agrego el mismo efecto de aviso exitoso al cambio de solucion provisoria

--- frontend/src/afectaciones/GestionSolucion.js
+++ frontend/src/afectaciones/GestionSolucion.js
@@ -3,6 +3,7 @@ import './css/Listas.css';
 
 function GestionSolucion({ cuenta, solucion_provisoria = [], telefonos = [], idafectacion, onGestionChange }) {
   const [selectedSolucion, setSelectedSolucion] = useState(solucion_provisoria[0] || '');
+  const [showSuccess, setShowSuccess] = useState(false);  // Estado para manejar el mensaje de éxito
 
   const handleSolucionChange = (event) => {
     const nuevaSolucion = event.target.value;
@@ -15,7 +16,12 @@ function GestionSolucion({ cuenta, solucion_provisoria = [], telefonos = [], ida
     .then(data => {
       if (data) {
         onGestionChange(nuevaSolucion);
-        setSelectedSolucion(''); // Restablecer el valor del select a vacío después del cambio
+        setShowSuccess(true);  // Mostrar mensaje de éxito
+
+        // Ocultar el mensaje de éxito después de 3 segundos
+        setTimeout(() => {
+          setShowSuccess(false);
+        }, 3000);
       }
     })
     .catch(error => console.error('Error:', error));
@@ -25,6 +31,7 @@ function GestionSolucion({ cuenta, solucion_provisoria = [], telefonos = [], ida
     <div>
       <div className="container-superior">
         <h3>Solucion Provisoria de la cuenta {cuenta}: </h3>
+
         <select id="solucionSelect" value={selectedSolucion} onChange={handleSolucionChange}>
           {solucion_provisoria.map((solucion, index) => (
             <option key={index} value={solucion}>
@@ -32,7 +39,31 @@ function GestionSolucion({ cuenta, solucion_provisoria = [], telefonos = [], ida
             </option>
           ))}
         </select>
+
+        {/* Mostrar el mensaje de éxito fuera del <select> */}
+        {showSuccess && (
+          <div className="success-message">
+            ¡Se cambió con éxito la Solución Provisoria!
+          </div>
+        )}
       </div>
+      
+      <style jsx>{`
+        .success-message {
+          margin-top: 10px;
+          padding: 10px;
+          background-color: #d4edda;
+          color: #155724;
+          border: 1px solid #c3e6cb;
+          border-radius: 5px;
+          animation: fade-in 0.5s ease-in-out;
+        }
+
+        @keyframes fade-in {
+          0% { opacity: 0; }
+          100% { opacity: 1; }
+        }
+      `}</style>
     </div>
   );
 }
