929928afaa5e5ef778d61bee235b017e5bb51d43
robertosl77 <robertosl77@gmail.com>
2024-10-14 20:39:51 -0300

    backend_1.4.13

--- backend/controladores/Controlador.py
+++ backend/controladores/Controlador.py
@@ -5,6 +5,7 @@ from servicios.ServicioCreaTablas import ServicioCreaTablas
 from servicios.ServicioAfectaciones import ServicioAfectaciones
 from servicios.ServiciosManager import ServiciosManager
 from servicios.ServicioGestor import ServicioGestor
+import re
 
 app = Flask(__name__)  # Cambié el nombre de la instancia Flask a 'app'
 CORS(app)
@@ -19,6 +20,10 @@ class Controlador:  # Cambié el nombre de la clase a 'ServiciosHandler'
         username = data.get('username')
         password = data.get('password')
         
+        if not validar_contrasena(password):
+            pass
+            # return jsonify({"success": False})
+
         authenticator = JsonValidador()
         user_data = authenticator.obtiene_usuario(username, password)
         
@@ -155,3 +160,46 @@ class Controlador:  # Cambié el nombre de la clase a 'ServiciosHandler'
         a = ServicioAfectaciones()
         # json = a.normalizar_afectacion()
         # return json, 200  
+
+
+
+
+def validar_contrasena(password):
+    # Verificar que la contraseña tenga al menos 8 caracteres
+    if len(password) < 8:
+        print("La contraseña debe tener al menos 8 caracteres.")
+        return False
+
+    # Verificar que contenga al menos una letra mayúscula
+    if not re.search(r'[A-Z]', password):
+        print("La contraseña debe contener al menos una letra mayúscula.")
+        return False
+
+    # Verificar que contenga al menos una letra minúscula
+    if not re.search(r'[a-z]', password):
+        print("La contraseña debe contener al menos una letra minúscula.")
+        return False
+
+    # Verificar que contenga al menos un número
+    if not re.search(r'\d', password):
+        print("La contraseña debe contener al menos un número.")
+        return False
+
+    # Verificar que contenga al menos un carácter especial
+    if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
+        print("La contraseña debe contener al menos un carácter especial.")
+        return False
+
+    # Verificar que no contenga números secuenciales (ej. 1234)
+    if re.search(r'(012|123|234|345|456|567|678|789|890)', password):
+        print("La contraseña no debe contener números secuenciales.")
+        return False
+
+    # Lista de contraseñas fáciles de adivinar
+    contrasenas_faciles = ["password", "12345678", "qwerty", "abc123", "admin", "user"]
+    if password.lower() in contrasenas_faciles:
+        print("La contraseña es demasiado fácil de adivinar.")
+        return False
+
+    print("La contraseña es válida.")
+    return True
