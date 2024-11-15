7730f54fdf52949dffce4c957e0a03130622758b
robertosl77 <robertosl77@gmail.com>
2024-11-15 15:29:39 -0300

    entregable5

--- backend/servicios/ServicioClientes.py
+++
@@ -1,9 +0,0 @@
-from bd.Datos import Datos
-
-class ServicioClientes:
-    def __init__(self) -> None:
-        pass
-
-    def obtiene_clientes(self):
-        d = Datos()
-        return d.obtiene_clientes()   
