40f0b33cae34517558a2a3059b6031aada9c7215
rsleiva <rsleiva@edenor.com>
2025-03-07 18:57:25 -0300

    frontend_1.9.8
    en el menu se subraya la vista activa.

--- frontend/src/navegacion/Navbar.js
+++ frontend/src/navegacion/Navbar.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom'; // Cambia Link por NavLink
 import './css/Navbar.css';
 import logo from './img/logo.png';
 
 function Navbar() {
-  const [isDropdownOpen, setDropdownOpen] = useState(false);  // Estado para manejar el menú desplegable
-  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false); // Estado para manejar el menú móvil
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
   const navigate = useNavigate();
   const username = sessionStorage.getItem('username');
   const rol = sessionStorage.getItem('rol');
-  
+
   const handleLogout = async () => {
     const response = await fetch('http://localhost:5000/SGE/Logout', {
       method: 'POST',
@@ -18,10 +18,10 @@ function Navbar() {
         'Content-Type': 'application/json',
       },
     });
-    
+
     if (response.ok) {
-      sessionStorage.clear();  // Limpiar el sessionStorage al desloguearse
-      navigate('/SGE/Login');  // Redirige al login después de desloguear
+      sessionStorage.clear();
+      navigate('/SGE/Login');
     }
   };
 
@@ -36,24 +36,24 @@ function Navbar() {
   useEffect(() => {
     const titleElement = document.querySelector('.navbar-title');
     if (!titleElement) return;
-  
+
     const updateTitle = () => {
-      titleElement.textContent = window.innerWidth <= 965 ? "SGE" : "Sistema de Gestión para Electrodependientes";
+      titleElement.textContent = window.innerWidth <= 1200 ? "SGE" : "Sistema de Gestión para Electrodependientes";
     };
-  
-    updateTitle(); // Ejecuta al cargar
-    window.addEventListener('resize', updateTitle); // Detecta cambios de tamaño
-  
-    return () => window.removeEventListener('resize', updateTitle); // Limpieza
-  }, []);  
+
+    updateTitle();
+    window.addEventListener('resize', updateTitle);
+
+    return () => window.removeEventListener('resize', updateTitle);
+  }, []);
 
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth > 520) {
-        setMobileMenuOpen(false); // Cerrar el menú si la pantalla es grande
+        setMobileMenuOpen(false);
       }
     };
-  
+
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
@@ -69,16 +69,25 @@ function Navbar() {
         </div>
       </div>
       <div>
-        {/* Botón para abrir el menú en pantallas pequeñas */}
         <button className="hamburger-menu" onClick={toggleMobileMenu}>
           ☰
         </button>
-
-        {/* Menú para pantallas grandes */}
         <ul className={`navbar-links ${isMobileMenuOpen ? 'open' : ''}`}>
-          {['admin', 'operador', 'consulta'].includes(rol) && <li><Link to="/SGE/Afectaciones">Afectaciones</Link></li>}
-          {['admin', 'operador'].includes(rol) && <li><Link to="/SGE/Climatica">Pronosticos Meteorologicos</Link></li>}
-          {['admin'].includes(rol) && <li><Link to="/SGE/Clientes">Clientes</Link></li>}
+          {['admin', 'operador', 'consulta'].includes(rol) && (
+            <li>
+              <NavLink to="/SGE/Afectaciones">Afectaciones</NavLink>
+            </li>
+          )}
+          {['admin', 'operador'].includes(rol) && (
+            <li>
+              <NavLink to="/SGE/Climatica">Pronosticos Meteorologicos</NavLink>
+            </li>
+          )}
+          {['admin'].includes(rol) && (
+            <li>
+              <NavLink to="/SGE/Clientes">Clientes</NavLink>
+            </li>
+          )}
           <li className="user-dropdown" onClick={toggleDropdown}>
             {username}
             {isDropdownOpen && (
