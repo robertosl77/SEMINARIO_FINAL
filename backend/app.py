c7bfe4e8711e46c5e0976e75cc57636c5dc4bec4
robertosl77 <robertosl77@gmail.com>
2025-03-08 00:31:10 -0300

    host

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,13 @@
 from controladores.Controlador import app
+import os
 
 class App:
     @staticmethod
     def run():
-        app.run(debug=True, port=5000, use_reloader=False) # Se asegura de que 'app' es la instancia correcta
+        # Obtener el puerto desde la variable de entorno PORT, o usar 5000 por defecto
+        port = int(os.getenv("PORT", 5000))
+        # Configurar el host en 0.0.0.0 para que sea accesible externamente
+        app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
 
 if __name__ == '__main__':
     App.run()
